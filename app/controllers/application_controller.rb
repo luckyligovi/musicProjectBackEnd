58d8b1bd81c6fd67ed931682f2c2ff4d74be2821
GordonGichini <gordongichini1@gmail.com>
2022-11-10 19:09:43 +0300

    post methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,30 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   # Add your routes here
-  get '/' do 
-    "Hello World"
-  end
+ # get '/' do 
+    
+ # end
   get "/songs" do
     songs = Song.all
     songs.to_json
     # (include: [:albums, :artists, :genres, :reviews])
   end
-get '/songs/:id' do
-  song = Song.find(params[:id])
-  song.to_json
-  # (include: [:albums, :artists, :genres, :reviews])
- end
-# method to delete song item
- delete '/songs/:id' do
-  song = Song.find(params[:id])
-  song.destroy
-  # (include: [:albums, :artists, :genres, :reviews])
- end
+  get '/songs/:id' do
+    song = Song.find(params[:id])
+    song.to_json
+    # (include: [:albums, :artists, :genres, :reviews])
+  end
+  # method to delete song item
+  delete '/songs/:id' do
+    song = Song.find(params[:id])
+    song.destroy
+    # (include: [:albums, :artists, :genres, :reviews])
+  end
 
-# method to handle a post request
+  # method to handle a post request
+  post '/songs' do 
+   Song.create(params)
+    redirect "/songs" 
+
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,8 @@ superstar = Album.create(name: "Superstar" )
 sip = Album.create(name: "Sip (Alcohol)" )
 sukari = Album.create(name: "Sukari" )
 utawezana = Album.create(name: "Utawezana")
+its only me = Album.create(name: "Its only me")
+Legend = Album.create(name: "Legend")
 
 
 puts "Creating artists..."
@@ -19,12 +21,16 @@ joeboy = Artist.create(full_name: "Joeboy")
 mejja = Artist.create(full_name: "Mejja Okonkwo")
 diamond = Artist.create(full_name: "Diamond Platnumz")
 zuchu = Artist.create(full_name: "Zuchu")
+lilbaby = Artist.create(full_name: "Lil Baby")
+Bobmarley = Artist.create(full_name: "Bob Marley")
 
 
 puts "Creating genres..."
 bongo = Genre.create(name: "Bongo")
 nigerian = Genre.create(name: "Nigerian")
 genge = Genre.create(name: "Genge")
+hiphop = Genre.create(name: "Hiphop")
+reggae = Genre.create(name: "Reggae")
 
 puts "Creating songs..."
 
@@ -32,6 +38,10 @@ song1 = Song.create(title: "Superstar", description: "Costa Titch and Diamond Pl
 song2 = Song.create(title: "Sip (Alcohol)", description: "Song by Joeboy" , img_url: "https://www2.beatzjam.com/wp-content/uploads/2021/10/0-31.jpg", artist_id: joeboy.id, genre_id: nigerian.id, album_id: sip.id)
 song3 = Song.create(title: "Sukari", description: "Song by Zuchu", img_url: "https://bekaboy.com/wp-content/uploads/2021/01/Sukari-VIDEO-640x321.jpg", artist_id: zuchu.id, genre_id: bongo.id, album_id: sukari.id)
 song4 = Song.create(title: "Utawezana", description: "Femi One X Mejja", img_url: "https://notjustok.com/eastafrica/wp-content/uploads/sites/3/2020/04/Femi-One-ft.-Mejja-Utawezana.png", artist_id: mejja.id, genre_id: genge.id, album_id: utawezana.id)
+song5 = Song.create(title: "Heyy", description: "Lil Baby", img_url: "https://i0.wp.com/hip-hopvibe.com/wp-content/uploa…/IMG_7678_800x440-e1665430839598.jpeg?w=800&ssl=1", artist_id: lilbaby.id, genre_id: hiphop.id, album_id: itsonlyme.id)
+song6 = Song.create(title: "Perfect Timing", description: "Lil Baby X Nardo Wick", img_url: "https://i0.wp.com/hip-hopvibe.com/wp-content/uploa…/IMG_7678_800x440-e1665430839598.jpeg?w=800&ssl=1", artist_id: lilbaby.id, genre_id: hiphop.id, album_id: itsonlyme.id)
+song7 = Song.create(title: "No Woman, No cry", description: "Vincent Ford", img_url: "https://www.jah-lyrics.com/artwork/albums/553.jpg", artist_id: bobmarley.id, genre_id: reggae.id, album_id: legend.id)
+song8 = Song.create(title: "Three Little Birds", description: "Bob Marley", img_url: "https://www.jah-lyrics.com/artwork/albums/553.jpg", artist_id: bobmarley.id, genre_id: reggae.id, album_id: legend.id)
 
 
 
@@ -52,6 +62,9 @@ Review.create(rating: 1, song_id: song1.id, comment: "Terrible")
 Review.create(rating: 2, song_id: song2.id, comment: "I had to mute")
 Review.create(rating: 5, song_id: song1.id, comment: "I listen every day")
 Review.create(rating: 1, song_id: song1.id, comment: "What is this?")
+Review.create(rating: 4, song_id: song7.id, comment: "Wow") 
+Review.create(rating: 5, song_id: song6.id, comment: "Best song") 
+Review.create(rating: 5, song_id: song8.id, comment: "Fantastic")
 
 
 
