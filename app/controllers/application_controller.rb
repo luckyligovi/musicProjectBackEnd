e9f5471f947552f55ef06d71e44c960ff4f12fe3
GordonGichini <gordongichini1@gmail.com>
2022-11-11 10:48:05 +0300

    added routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,8 +23,116 @@ class ApplicationController < Sinatra::Base
 
   # method to handle a post request
   post '/songs' do 
-   Song.create(params)
+   Song.create(
+    title:params[:title],
+    description:params[:description],
+    img_url:params[:img_url],
+    artist_id: artist.id,
+    genre_id: genre.id,
+    album_id: album.id
+  )
     redirect "/songs" 
 
   end
+
+  # Artist methods 
+  get "/artists" do
+    artists = Artist.all
+    artists.to_json
+    # (include: [:albums, :artists, :genres, :reviews])
+  end
+  get '/artists/:id' do
+    artist = Artist.find(params[:id])
+    artist.to_json
+    # (include: [:albums, :artists, :genres, :reviews])
+  end
+  # method to delete song item
+  delete '/artists/:id' do
+    artist = Artist.find(params[:id])
+    artist.destroy
+    # (include: [:albums, :artists, :genres, :reviews])
+  end
+
+  # method to handle a post request
+  post '/artists' do 
+   Artist.create(full_name:params[:full_name])
+    redirect "/artists" 
+
+  end
+  #Genre methods
+  get "/genres" do
+    genres = Genre.all
+    genres.to_json
+    # (include: [:albums, :artists, :genres, :reviews])
+  end
+  get '/genres/:id' do
+    genre = Genre.find(params[:id])
+    genre.to_json
+    # (include: [:albums, :artists, :genres, :reviews])
+  end
+  # method to delete song item
+  delete '/genres/:id' do
+    genre = Genre.find(params[:id])
+    genre.destroy
+    # (include: [:albums, :artists, :genres, :reviews])
+  end
+
+  # method to handle a post request
+  post '/genres' do 
+   Genre.create(name:params[:name])
+    redirect "/genres" 
+
+  end
+  #Album methods
+  get "/albums" do
+    albums = Album.all
+    albums.to_json
+    # (include: [:albums, :artists, :genres, :reviews])
+  end
+  get '/albums/:id' do
+    album = Album.find(params[:id])
+    album.to_json
+    # (include: [:albums, :artists, :genres, :reviews])
+  end
+  # method to delete song item
+  delete '/albums/:id' do
+    album = Album.find(params[:id])
+    album.destroy
+    # (include: [:albums, :artists, :genres, :reviews])
+  end
+
+  # method to handle a post request
+  post '/albums' do 
+   Album.create(name:params[:name])
+    redirect "/albums" 
+
+  end
+#Review methods
+get "/reviews" do
+  reviews = Review.all
+  reviews.to_json
+  # (include: [:albums, :artists, :genres, :reviews])
+end
+get '/reviews/:id' do
+  review = Review.find(params[:id])
+  review.to_json
+  # (include: [:albums, :artists, :genres, :reviews])
+end
+# method to delete song item
+delete '/reviews/:id' do
+  review = Review.find(params[:id])
+  review.destroy
+  # (include: [:albums, :artists, :genres, :reviews])
+end
+
+# method to handle a post request
+post '/reviews' do 
+ Review.create(
+  rating:params[:rating],
+  song_id:song.id,
+  comment:params[:comment]
+)
+  redirect "/reviews" 
+end
+
 end
